343dfda3d421d5c37096aee6255732dd534ef52d
Brian <dpi.studio@gmail.com>
2023-03-16 00:06:09 +0800

    real final version

--- demoSignPdfMerger/Program.cs
+++ demoSignPdfMerger/Program.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using Aspose.Pdf;
 using Aspose.Pdf.Facades;
-using System.Security.Cryptography.X509Certificates;
 using Aspose.Pdf.Forms;
-using System.Xml.Linq;
-using System.Collections.Generic;
 
 namespace demoSignPdfMerger
 {
@@ -15,39 +11,29 @@ namespace demoSignPdfMerger
         static string outputFile = @"C:\dev\_tmp\output\merged.pdf";
         static string signFile = @"C:\dev\_tmp\Brian.pfx";
         static string signPassword = "P@ssw0rd";
-        static string[] inputFiles = Directory.GetFiles(inputPath, "*.pdf");
 
         static void Main(string[] args)
         {
+            string[] inputFiles = Directory.GetFiles(inputPath, "*.pdf");
             using (Document outputDocument = new Document())
             {
                 foreach (string inputFile in inputFiles)
                 {
                     using (Document inputDocument = new Document(inputFile))
-                    {
-                        Console.WriteLine($"Read file: {inputFile}:");
                         outputDocument.Pages.Add(RemoveSign(inputDocument).Pages);
-                    }
                 }
                 outputDocument.Save(outputFile);
             }
             ResignAndSave(outputFile);
-            Console.WriteLine($"Output: {outputFile}:");
-            Console.ReadKey();
         }
 
         static Document RemoveSign(Document inputDocument)
         {
-            // verify sign
             PdfFileSignature pdfSign = new PdfFileSignature();
             pdfSign.BindPdf(inputDocument);
-            //Console.WriteLine(pdfSign.VerifySignature("Signature2"));
             var sigNames = pdfSign.GetSignNames();
-            // remove sign
             foreach (var sigName in sigNames)
-            {
                 pdfSign.RemoveSignature(sigName);
-            }
             return inputDocument;
         }
 
