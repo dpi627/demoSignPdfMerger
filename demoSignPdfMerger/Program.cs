0f094eb5452f58f39d0b47dc56efad7ac3876957
Brian <dpi.studio@gmail.com>
2023-03-15 20:37:20 +0800

    rfc: use using-block mic: add/rewrite some comment mic: remove unnessary using

--- demoSignPdfMerger/Program.cs
+++ demoSignPdfMerger/Program.cs
@@ -15,22 +15,27 @@ namespace demoSignPdfMerger
             string inputPath = @"C:\dev\_tmp";
             string outputFile = @"C:\dev\_tmp\output\merged.pdf";
 
-            // Get a list of all PDF files in the input path
+            // Get all PDF in input path
             string[] inputFiles = Directory.GetFiles(inputPath, "*.pdf");
-            // Create Document object for output PDF
-            Document outputDocument = new Document();
-            foreach (string inputFile in inputFiles)
-            {
-                // Create a new Document object for the input PDF file
-                Document inputDocument = new Document(inputFile);
 
-                // Output the extracted text to the console
-                Console.WriteLine($"Read file: {inputFile}:");
-                outputDocument.Pages.Add(inputDocument.Pages);
+            // Create Document object for output PDF
+            using (Document outputDocument = new Document())
+            {
+                // Loop all input files
+                foreach (string inputFile in inputFiles)
+                {
+                    // Create Document object for input PDF
+                    using (Document inputDocument = new Document(inputFile))
+                    {
+                        Console.WriteLine($"Read file: {inputFile}:");
+                        // add all pages
+                        outputDocument.Pages.Add(inputDocument.Pages);
+                    }
+                }
+                // Save output document
+                outputDocument.Save(outputFile);
+                Console.WriteLine($"Output: {outputFile}:");
             }
-            // Save output document
-            outputDocument.Save(outputFile);
-            Console.WriteLine($"Output: {outputFile}:");
             Console.ReadKey();
         }
     }
